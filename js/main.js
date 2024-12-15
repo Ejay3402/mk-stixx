c698fecb39b76a8dd527dc167c486bfc5997e4a7
Elijah Jacob <elijahjacob3402@gmail.com>
2024-12-15 13:56:38 +0100

    still working

--- js/main.js
+++ js/main.js
@@ -6,9 +6,21 @@ let src1F = false;
 setInterval( () => {
 
     if (src1F) {
-        src1.setAttribute('src', 'assets/images/ml-stixx.JPG');
+        src1.setAttribute('src', 'assets/images/ml-stixx.png');
     } else {
         src1.setAttribute('src', 'assets/images/mk2.PNG');
     }
     src1F = !src1F;
 }, 5000 )
+
+//open and close meun
+
+const open_meun = document.querySelector(`.open-meun`);
+const close_meun = document.querySelector(`.close-meun`);
+const ul = document.querySelector(`.ul`);
+
+
+open_meun.addEventListener("click", () => ul.style.right = '0');
+
+close_meun.addEventListener("click", () => ul.style.right = '-400px');
+
